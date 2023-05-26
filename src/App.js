a0acaeabd2412ad694cf229d283cc6072852db8b
sidram403 <siddharthpatil9108@gmail.com>
2023-05-27 02:07:30 +0530

    Todo List application created by using React.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const editTodo = todos.find((item) => item.id === editId);
+      const updateTodo = todos.map((item) =>
+        item.id === editTodo.id
+          ? (item = { id: item.id, todo })
+          : { id: item.id, todo: item.todo }
+      );
+      setTodos(updateTodo);
+      setEditId(0);
+      setTodo("");
+      return ;
+    }
+
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+    console.log(todos);
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((item) => item.id !== id);
+    setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((item) => item.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="app">
+        <div className="container">
+          <h1>Todo List App</h1>
+          <form className="todoForm" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Add to list..."
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+            />
+            <button type="submit">{editId ? "Edit" : "Add"}</button>
+          </form>
+
+          <ul className="allTodo">
+            {todos.map((item) => (
+              <li key={item.id}>
+                <span className="singleTodo">{item.todo}</span>
+                <button onClick={() => handleEdit(item.id)}>Edit</button>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
